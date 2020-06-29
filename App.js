2e3256871ee265c0fbfdf433cef774a0db37e91b
Sebastian Guerrero <juanse0421@gmail.com>
2020-06-28 19:59:38 -0500

    WiFi testing pass

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ import {
   View,
   Text,
   StatusBar,
+  PermissionsAndroid
 } from 'react-native';
+
 
 import Login from './Views/Login';
 import OpenAPP from './Views/OpenAPP';
@@ -14,9 +16,26 @@ import Register from './Views/Register';
 import Service from './Views/Service';
 
 
+
 const App = () => {
   let [view,setView]=useState(<OpenAPP/>)
-  useEffect(()=>{
+  useEffect(async ()=>{
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location permission is required for WiFi connections',
+        message:
+          'This app needs location permission as this is required  ' +
+          'to scan for wifi networks.',
+        buttonNegative: 'DENY',
+        buttonPositive: 'ALLOW',
+      },
+);
+if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    // You can now use react-native-wifi-reborn
+} else {
+    // Permission denied
+}
     setTimeout(()=>{setView(<Login
       openRegister={()=>{
         register()
@@ -49,6 +68,8 @@ const App = () => {
     function service(){
       setView(<Service/>)
     }
+
+    
   },[])
 
   return (
